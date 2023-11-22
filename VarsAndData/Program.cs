cd9bb2d8e4f4065cc4c75236d2731adc10728016
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 07:20:55 +0000

    VarsAndData

---
+++ VarsAndData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VarsAndData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float f = 2.0f;
+            decimal d = 10.0m;
+            bool b = true;
+            char c = 'c';
+
+            string str = "a string";
+
+            var x = 10;
+            var z = "Hello!";
+
+            int[] vals = new int[5];
+            string[] strs = {"one", "two", "three"};
+
+            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}",i,c,b,str,f,d,x,z);
+        }
+    }
+}
