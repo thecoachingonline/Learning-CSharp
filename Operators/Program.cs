20be0a1b6aa817bc3218722a0593f9984cc37721
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 07:57:00 +0000

    +,-,*,/

---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 5;
+            string a = "abcd", b = "efgh";
+
+            Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
+
+            Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            a += b;
+            Console.WriteLine(a);
+        }
+    }
+}
