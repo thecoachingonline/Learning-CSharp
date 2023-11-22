811b762869aab8418975b94b230176df2316e53b
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 10:37:15 +0000

    if-else

---
+++ Program Flow/Conditional-if/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 51;
+
+            if (theVal == 50) {
+                Console.WriteLine("theVal is 50");
+            }
+            else if (theVal >= 51 && theVal <= 60)
+            {
+                Console.WriteLine("theVal is between 51 and 60");
+            }
+            else {
+                Console.WriteLine("theVal is something else");
+            }
+            Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
+        }
+    }
+}
