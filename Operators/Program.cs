e4256bf413fd64d48ef5368a4eb8378839fd9c0d
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 09:33:53 +0000

    Operators

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -9,18 +9,28 @@ namespace Operators
             int x = 10, y = 5;
             string a = "abcd", b = "efgh";
 
-            Console.WriteLine("----- Basic Math -----");
-            Console.WriteLine((x / y) * x);
-            Console.WriteLine(a + b);
+            // Console.WriteLine("----- Basic Math -----");
+            // Console.WriteLine((x / y) * x);
+            // Console.WriteLine(a + b);
 
             Console.WriteLine("----- Shorthand -----");
             x++;
             y--;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
 
-            a += b;
-            Console.WriteLine(a);
+            // a += b;
+            // Console.WriteLine(a);
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
+
+            string str = null;
+
+            Console.WriteLine(str ?? "Unknown string");
+
+            str ??= "New String";
+            Console.WriteLine(str);
+
         }
     }
 }
