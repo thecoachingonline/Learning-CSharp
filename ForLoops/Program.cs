ab47bfcc93e9448db0ade3b5e3b3cabc816cba14
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 02:24:09 +0000

    For loops

--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -10,11 +10,22 @@ namespace ForLoops
             int[] nums = new int[] {3, 14, 15, 92, 6};
             string str = "The quick brown fox jumps over the lazy dog";
 
-            Console.WriteLine("The basic for loop:");
-            for (int i = 0; i < myVal; i++) {
-                Console.WriteLine("i is currently {0}", i);
-            }
+         //   Console.WriteLine("The basic for loop:");
+         //   for (int i = 0; i < myVal; i++) {
+         //       Console.WriteLine("i is currently {0}", i);
+         //   }
             Console.WriteLine();
+
+        //    foreach (int i in nums) {
+        //        Console.WriteLine("i is currently {0}", i);
+        //    }
+            var count = 0;
+            foreach (char c in str) {
+                if (c == 'o') {
+                    count++;
+                }
+            }
+            Console.WriteLine("Counted {0} o character", count);
         }
     }
 }
