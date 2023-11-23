01c4db6ede7852b98d1ae7b2ca6d4579fc763ca5
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 09:17:40 +0000

    String

---
+++ Strings/Operations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outstr;
+            string str1 = "The quick brown fox jumps over the lazy dog.";
+            string str2 = "This is a string";
+            string str3 = "THIS is a STRING";
+            string[] strs = {"one", "two", "three", "four"};
+
+            Console.WriteLine(str1.Length);
+
+            Console.WriteLine(str1[14]);
+
+            foreach (char ch in str1) {
+                Console.Write(ch);
+                if (ch == 'b')
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+    }
+}
