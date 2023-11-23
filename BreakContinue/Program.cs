8b34357ab6931917730181658b7f673f6e0e6ab5
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 03:31:17 +0000

    Break

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = {15, 7, 12, 23, 41, 28, 9, 17, 36};
+
+            Console.WriteLine("Using break and continue :");
+            foreach (int val in values)
+            {
+                Console.WriteLine($"val is currently {val}");
+
+                if (val >= 40) {
+                    break;
+                }
+            }
+        }
+    }
+}
