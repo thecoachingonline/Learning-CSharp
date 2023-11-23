3f62e70a70ef4c9629c1fb90b71ece94d769a958
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 00:16:51 +0000

    Loop

---
+++ ForLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVal = 15;
+            int[] nums = new int[] {3, 14, 15, 92, 6};
+            string str = "The quick brown fox jumps over the lazy dog";
+
+            Console.WriteLine("The basic for loop:");
+            for (int i = 0; i < myVal; i++) {
+                Console.WriteLine("i is currently {0}", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
