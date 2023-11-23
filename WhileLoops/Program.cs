2fb05a476fa3c216e2ed2986bf049e1ec520d771
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 02:39:51 +0000

    while loop

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "";
+
+            Console.WriteLine("Basic while() loop:");
+            while (inputStr != "exit") {
+                inputStr = Console.ReadLine();
+                Console.WriteLine("You entered: {0}", inputStr);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
