4075e7323eba272f5621289231d65745111eb89c
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-26 02:36:41 +0000

    String Formatting

--- Strings/Formatting/Program.cs
+++ Strings/Formatting/Program.cs
@@ -14,6 +14,20 @@ namespace Formatting
         decimal val2 = 1234.5678m;
 
         Console.WriteLine("{0}", str1);
+
+        Console.WriteLine("{0:D}, {0:N}, {0:F}, {0:G}", val1);
+        Console.WriteLine("{0:E}, {0:N}, {0:F}, {0:G}", val2);
+
+        Console.WriteLine("{0:D6} {0:N2} {0:F1} {0:G3}", val1);
+
+        Console.WriteLine("{0,12} {1,12} {2,12} {3,12}",
+                quarters[0],quarters[1],quarters[2],quarters[3]);
+
+        Console.WriteLine("{0,12:C0} {1,12:C0} {2,12:C0} {3,12:C0}",
+                sales[0],sales[1],sales[2],sales[3]);
+
+        Console.WriteLine("{0,12:P0} {1,12:P0} {2,12:P1} {3,12:P2}",
+                intIMixPct[0],intIMixPct[1],intIMixPct[2],intIMixPct[3]);
     }
    } 
 }
