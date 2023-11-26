9635a5b703b19f9f57c61ce36ababbec0f70c293
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-26 07:14:16 +0000

    String Builder

--- Strings/Builder/Program.cs
+++ Strings/Builder/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jumpCount = 10;
+            string[] animals = {"goats", "cats", "pigs"};
+
+            StringBuilder sb = new StringBuilder("Initial string.", 200);
+
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+        }
+    }
+}
