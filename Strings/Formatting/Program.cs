013133c9ef7a0ebbf60484a60df4583e04fcf0c3
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-26 01:50:13 +0000

    Basic Formatting

--- Strings/Formatting/Program.cs
+++ Strings/Formatting/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Formatting
+{
+   class Program
+   {
+    static void Main(string[] args)
+    {
+        int[] quarters = {1,2,3,4};
+        int[] sales = {100000, 150000, 200000, 225000};
+        double[] intIMixPct = {.386, .413, .421, .457};
+        string str1 = "TestStr";
+        int val1 = 1234;
+        decimal val2 = 1234.5678m;
+
+        Console.WriteLine("{0}", str1);
+    }
+   } 
+}
