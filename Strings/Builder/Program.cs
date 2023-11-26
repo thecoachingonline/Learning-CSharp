4ba470a07ba7202ade9ba2d6b4339361fcace95c
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-26 09:49:52 +0000

    Using String Builder

--- Strings/Builder/Program.cs
+++ Strings/Builder/Program.cs
@@ -13,6 +13,24 @@ namespace Builder
             StringBuilder sb = new StringBuilder("Initial string.", 200);
 
             Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+
+            sb.Append("The quick brown fox");
+            sb.Append("Jumped over the lazy dog.");
+
+            sb.AppendLine();
+
+            sb.AppendFormat("He did this {0} times.", jumpCount);
+            sb.AppendLine();
+
+            sb.Append("He also jumped over ");
+            sb.AppendJoin(',', animals);
+
+            sb.Replace("fox", "cat");
+
+            sb.Insert(0, "This is the ");
+
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+            Console.WriteLine(sb.ToString());
         }
     }
 }
