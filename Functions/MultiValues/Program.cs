26f0a8f945c6845dfef98156424021a4a6f4dffd
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-27 14:49:32 +0000

    Returning multiple values

--- Functions/MultiValues/Program.cs
+++ Functions/MultiValues/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MultiValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int a, int b) tup1 = (5,10);
+            var tup2 = ("Some text", 10.5f);
+
+            tup1.b = 20;
+            tup2.Item1 = " New String";
+            Console.WriteLine($"{tup1.a}, {tup1.b}");
+            Console.WriteLine($"{tup2.Item1}, {tup2.Item2}");
+
+            (int, int) result = PlusTimes(6, 12);
+            Console.WriteLine($"Sum: {result.Item1}, Product: {result.Item2}");
+        }
+
+        static (int, int) PlusTimes(int a, int b) {
+            return (a+b, a*b);
+        }
+    }
+}
