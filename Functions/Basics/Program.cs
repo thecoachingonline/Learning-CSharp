4da9be91c465641b213682a07e702af1769aa485
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-27 08:58:52 +0000

    Function basics

--- Functions/Basics/Program.cs
+++ Functions/Basics/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+float MilesToKm(float miles) {
+    float result = miles * 1.6f;
+    return result;
+}
+
+void PrintWithPrefix(string theStr) {
+    Console.WriteLine($"::> {theStr}");
+}
+
+Console.WriteLine($"The result is {MilesToKm(8.0f)}");
+Console.WriteLine($"The result is {MilesToKm(52.0f)}");
+
+PrintWithPrefix("Test string");
+PrintWithPrefix("Another Test String");
