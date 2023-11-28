c9f279c406888bcae2080118dcab1e4fdb2fa65c
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-28 07:20:00 +0000

    String Representation

--- Classes/StringRep/Program.cs
+++ Classes/StringRep/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StringRep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000;
+            Console.WriteLine(x.ToString());
+
+        //    object a = new object();
+        //    Console.WriteLine(a.ToString());
+
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Console.WriteLine(b1.ToString());
+            Console.WriteLine(b1);
+
+            Console.WriteLine(b1.ToString('B'));
+            Console.WriteLine(b1.ToString('F'));
+        }
+    }
+}
