3c19994ebce069db933ff63e93db19213e98cbd0
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-28 04:36:30 +0000

    Access Modifiers

--- Classes/Modifiers/Program.cs
+++ Classes/Modifiers/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("war and Peace", "Leo Tolstoy", 825);
+
+        //    b1._name = "Cat in the Hat";
+        //    Console.WriteLine(b1.GetDescription());
+
+            b1.SetName("Grapes of Wrath");
+            b1.SetAuthor("John Steinbeck");
+            b1.SetPagecount(464);
+            Console.WriteLine(b1.GetDescription());
+        }
+    }
+}
