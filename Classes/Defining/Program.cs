4c51eff97e1b280297ec52578e19e13877226016
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-28 03:22:47 +0000

    Defining C# classes

--- Classes/Defining/Program.cs
+++ Classes/Defining/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Defining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Book b2 = new Book("The Grapes of Wrath", "John Steinbeck", 464);
+
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+
+            b1._name = "Aldous Huxley";
+        }
+    }
+}
