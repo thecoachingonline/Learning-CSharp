d4f1964e264e21bd605c040a33cd2bfe25f4baa9
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-02 08:26:14 +0000

    First

---
+++ mySparkApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Spark.Sql
+
+namespace MySparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession
+                .Builder()
+                .AppName("world_count_sample")
+                .GetOrCreat();
+
+            DataFrame dataFrame = spark.Read().text("input.txt");
+
+            var world = dataFrame
+                .Select(Function.Split(Function.Col("value"), "").Alias("words"))
+                .Select(Function.Explode(Function.Col("world"))
+                .Alias("word"))
+                .GroupBy("word")
+                .Count()
+                .OrderBy(Function.Col("count").Desc());
+
+                words.show();
+        }
+    }
+}
