ad543aa244dd6d093b999507e5ff78a343798dd6
Y.Nanthachai <thecoachingonline@gmail.com>
2024-07-03 01:50:32 +0000

    Visual Studio 2008

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ทักทายผู้ใช้งาน
+        string strRead = "";
+        Console.Write("Please insert your name: ");
+        strRead = Console.ReadLine();
+        Console.WriteLine("Hello! Mr./Mss. " + strRead);
+        Console.WriteLine(" Date -> " + DateTime.Now.ToShortDateString());
+        Console.WriteLine(" Time -> " + DateTime.Now.ToShortTimeString());
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit program");
+        Console.ReadLine();
+    }
+}
