8f638e5ad32b5a4c210eb124011452c025f5d23c
Y.Nanthachai <thecoachingonline@gmail.com>
2025-03-07 03:27:47 +0000

    .NET 8

--- Overview/HelloWorld/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
+++ Overview/HelloWorld/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749607e4a120e884234e28590c3c68a238ba27b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c65e4b7a2d2b7afc8170f40f5e867435538b17")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
